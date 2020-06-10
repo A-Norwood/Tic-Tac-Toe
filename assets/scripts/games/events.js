3ef7d1742460510ee086876d8a0ebfdb63af2186
Ashley Norwood <norwood54@icloud.com>
2020-06-10 16:52:01 -0400

    Was able declare a winner, if there's a tie. Need to make it where game board clears after win, and make button work for new game. Need to declare who's turn it is.

--- assets/scripts/games/events.js
+++ assets/scripts/games/events.js
@@ -2,7 +2,7 @@ const api = require('./api')
 const ui = require('./ui')
 const store = require('../store.js')
 
-const getFormFields = require('../../../lib/get-form-fields.js')
+// const getFormFields = require('../../../lib/get-form-fields.js')
 
 const onCreateGame = function (event) {
   // console.log(user.token)
@@ -13,13 +13,14 @@ const onCreateGame = function (event) {
     .catch(ui.createFailure)
 }
 
-let currentPlayer = 'X'
+let currentPlayer = 'O'
 
 const onUpdateGame = function (event) {
   event.preventDefault()
   // console.log(event.target.id)
+
   if (store.game.cells[event.target.id] === 'X' || store.game.cells[event.target.id] === 'O') {
-    alert('Invalid Move!')
+    return $('.message').text('Invalid Move!').fadeOut(4000)
   }
 
   // updates game cells using current index
@@ -34,12 +35,45 @@ const onUpdateGame = function (event) {
     currentPlayer = 'X'
   }
 
+  winner()
+
   api.updateGame()
     .then(ui.updateGameSuccess)
     .catch(ui.updateGameFailure)
 }
 
+
+const winner = function () {
+  if (store.game.cells[0] === store.game.cells[1] && store.game.cells[1] === store.game.cells[2] && store.game.cells[0] !== '') {
+    return $('.message').text('Winner is ' + store.game.cells[0])
+  } else if (store.game.cells[3] === store.game.cells[4] && store.game.cells[4] === store.game.cells[5] && store.game.cells[3] !== '') {
+    return $('.message').text('Winner is ' + store.game.cells[3])
+  } else if (store.game.cells[6] === store.game.cells[7] && store.game.cells[7] === store.game.cells[8] && store.game.cells[6] !== '') {
+    return $('.message').text('Winner is ' + store.game.cells[6])
+  } else if (store.game.cells[0] === store.game.cells[4] && store.game.cells[4] === store.game.cells[8] && store.game.cells[0] !== '') {
+    return $('.message').text('Winner is ' + store.game.cells[0])
+  } else if (store.game.cells[2] === store.game.cells[4] && store.game.cells[4] === store.game.cells[6] && store.game.cells[2] !== '') {
+    return $('.message').text('Winner is ' + store.game.cells[2])
+  } else if (store.game.cells[0] === store.game.cells[3] && store.game.cells[3] === store.game.cells[6] && store.game.cells[0] !== '') {
+    return $('.message').text('Winner is ' + store.game.cells[0])
+  } else if (store.game.cells[1] === store.game.cells[4] && store.game.cells[4] === store.game.cells[7] && store.game.cells[1] !== '') {
+    return $('.message').text('Winner is ' + store.game.cells[1])
+  } else if (store.game.cells[2] === store.game.cells[5] && store.game.cells[5] === store.game.cells[8] && store.game.cells[2] !== '') {
+    return $('.message').text('Winner is ' + store.game.cells[2])
+  }
+  let isATie = true
+  for (let i = 0; i < store.game.cells.length; i++) {
+    if (store.game.cells[i] === '') {
+      isATie = false
+    }
+  }
+  if (isATie === true) {
+    return $('.message').text('Its a tie!')
+  }
+}
+
 module.exports = {
   onCreateGame,
   onUpdateGame
+  // winner
 }
